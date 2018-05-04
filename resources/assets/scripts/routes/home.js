14e6a983df0fb24383f2527147b99ad31eb895c5
Meet Godhani <meetgodhani23@gmail.com>
2018-05-03 21:04:13 -0400

    Updates

--- resources/assets/scripts/routes/home.js
+++ resources/assets/scripts/routes/home.js
@@ -1,6 +1,21 @@
+import AOS from 'aos'
+
 export default {
   init() {
-    // JavaScript to be fired on the home page
+    const os = navigator;
+
+    AOS.init()
+
+    $('#downloadModal').on('show.bs.modal', function () {
+      $('#download')[0].click();
+    })
+    if (os.platform === "Win32") {
+        $("#download, #downloadstepmob, #downloadstep").attr("href", "https://github.com/ridereceipts/ridereceipts/releases/download/v1.3.0/Ride.Receipts.Setup.1.3.0.exe")
+    } else if (os.platform === "MacIntel") {
+      $("#download, #downloadstepmob, #downloadstep").attr("href", "https://github.com/ridereceipts/ridereceipts/releases/download/v1.3.0/Ride.Receipts-1.3.0-mac.zip")
+    } else {
+      $("#download, #downloadstepmob, #downloadstep").attr("href", "https://github.com/ridereceipts/ridereceipts/releases/download/v1.3.0/ridereceipts-1.3.0-x86_64.AppImage")
+    }
   },
   finalize() {
     // JavaScript to be fired on the home page, after the init JS
